147030b9b89469180faaef53705851d6fa3998ff
jorgeucano <jorgeucano@gmail.com>
2016-06-14 09:42:48 -0300

    first commit

---
+++ platforms/ios/build/chatAleman.build/Debug-iphonesimulator/chatAleman.build/Objects-normal/i386/main.d
@@ -0,0 +1,8 @@
+dependencies: \
+  /Users/jorgeucano/Documents/newProyect/chatAleman/platforms/ios/internal/main.m \
+  /Users/jorgeucano/Documents/newProyect/chatAleman/platforms/ios/internal/NativeScript/include/NativeScript.h \
+  /Users/jorgeucano/Documents/newProyect/chatAleman/platforms/ios/internal/NativeScript/include/TNSRuntime.h \
+  /Users/jorgeucano/Documents/newProyect/chatAleman/platforms/ios/internal/NativeScript/include/TNSRuntime+Diagnostics.h \
+  /Users/jorgeucano/Documents/newProyect/chatAleman/platforms/ios/internal/NativeScript/include/TNSRuntime+Inspector.h \
+  /Users/jorgeucano/Documents/newProyect/chatAleman/platforms/ios/internal/TNSExceptionHandler.h \
+  /Users/jorgeucano/Documents/newProyect/chatAleman/platforms/ios/internal/TNSDebugging.h
